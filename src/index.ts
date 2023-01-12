f2b1a4c4d56d326605888be81b119d57ee571154
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-01-12 15:17:18 -0300

    build: swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 /* eslint-disable prettier/prettier */
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
+import swaggerDocument from "../swagger.json";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/users", usersRoutes);
 
